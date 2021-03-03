5dbe947c1880abd678f03b04e7140040e1051db1
Isabella Paine <idcpaine@gmail.com>
2021-03-03 04:18:58 +0000

    change syntax to quote instead of greeting

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -15,14 +15,15 @@
 /**
  * Adds a random greeting to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function addRandomQuote() {
+  const quotes =
+    ['Be comfortable with being uncomfortable', 'When you are confident, people become confident in you', 
+    'You get in life what you have the courage to ask for', 'Everyday is a fresh start', 'Take the risk or lose the chance'];
 
   // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const quoteContainer = document.getElementById('quote-container');
+  quoteContainer.innerText = quote;
 }
