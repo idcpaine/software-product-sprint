0b65f5cc6e644ce127abc0ac43301de0d5a73864
Isabella Paine <idcpaine@gmail.com>
2021-03-12 06:27:27 +0000

    fetch json list from server

--- portfolio/src/main/webapp/script2.js
+++ portfolio/src/main/webapp/script2.js
@@ -1,6 +1,12 @@
 async function showHello(){
     const responseFromServer = await fetch('/hello');
-    const textFromResponse = await responseFromServer.text();
+    const jsonFromResponse = await responseFromServer.json();
     const helloContainer = document.getElementById('hello-container');
-    helloContainer.innerText = textFromResponse;
+    const quote = jsonFromResponse[Math.floor(Math.random() * jsonFromResponse.length)];
+    // helloContainer.innerText = jsonFromResponse[2];
+    helloContainer.innerText = quote;
+
+    // const textFromResponse = await responseFromServer.text();
+    // const helloContainer = document.getElementById('hello-container');
+    // helloContainer.innerText = textFromResponse;
 }
